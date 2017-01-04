3692770aac3a75a55bbd7ae252a9039c805a1848
MsLarissaaa <larissapittenger@gmail.com>
2017-01-03 23:46:41 -0600

    Commit solution to PrimeFactors exercise.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,44 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        if(!primeFactors.isEmpty()){
+            System.out.print("The prime factor(s) of this number is/are: ");
+            for(int primeFact: primeFactors){
+                System.out.print(primeFact + " ");
+            }
+        } else {
+            System.out.println("Sorry, this number doesn't have any prime factors.");
+        }
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> all_factors = find_factors(n);
+        for(int j = all_factors.size() - 1; j >= 0; j--){
+            if(!is_prime(all_factors.get(j))){
+                all_factors.remove(j);
+            }
+        }
+        return all_factors;
+    }
+
+    private static boolean is_prime(int number){
+        List<Integer> factors = find_factors(number);
+        return ((factors.size() == 0) && (number > 1));
+    }
+
+    private static List<Integer> find_factors(int num) {
+        List<Integer> found_factors = new ArrayList<>();
+        for(int i = 2; i < num; i++){
+            if(num % i == 0){
+                found_factors.add(i);
+            }
+        }
+        return found_factors;
     }
 }
