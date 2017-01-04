3040792a47ea4663808f4dc31e44b2655de028c4
MsLarissaaa <larissapittenger@gmail.com>
2017-01-03 21:53:59 -0600

    Commit solutions for Diamond Exercises.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -13,6 +13,15 @@ public class DiamondExercises {
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
+        for (int i = 1; i < (n * 2); i +=2){
+            for (int k = 0; k < (n - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
     }
 
@@ -24,7 +33,24 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
+        for (int i = 1; i < (n * 2); i +=2){
+            for (int k = 0; k < (n - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = (n*2) - 3; i > 0; i -=2){
+            for (int k = 0; k < (n - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
 //    Diamond with Name
@@ -36,6 +62,28 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
+        for (int i = 1; i < (n * 2); i +=2){
+            for (int k = 0; k < (n - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            if(i == ((n * 2) - 1)) {
+                System.out.print("Larissa");
+            } else {
+                for (int j = 0; j < i; j++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+        for (int i = (n*2) - 3; i > 0; i -=2){
+            for (int k = 0; k < (n - (i / 2)); k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
 
     }
 }
